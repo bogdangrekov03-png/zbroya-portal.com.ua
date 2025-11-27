44205c7b30c7e96788dc2f77c36b2803a6a954ca
bogdangrekov03-png <bogdangrekov03@gmail.com>
2025-11-27 21:00:09 +0200

    Delete keyshop-frontend directory

--- keyshop-frontend/src/api.js
+++
@@ -1,7 +0,0 @@
-
-import axios from "axios";
-export const API_URL = "https://keyshop-backend-n11g.onrender.com";
-export const api = axios.create({
-  baseURL: API_URL,
-  headers: { Authorization: `Bearer ${localStorage.getItem("token") || ""}` },
-});
